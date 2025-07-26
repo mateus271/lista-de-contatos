cdee37c57f1dd6c4bd929026f5083addc26b806c
Hugo Patricio <hugopatricio51@gmail.com>
2025-07-26 16:06:16 -0300

    feat: adiciona uso do json server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
+import { CommonModule } from '@angular/common';
+import { ListComponent } from './features/list/list.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ListComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule,
+    CommonModule,
+    MatCardModule,
   ],
-  providers: [
-    provideClientHydration(withEventReplay())
-  ],
-  bootstrap: [AppComponent]
+  providers: [provideClientHydration(withEventReplay())],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface Contact {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+}
 
 @Component({
   selector: 'app-list',
   standalone: false,
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrl: './list.component.scss',
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  contacts: Contact[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<Contact[]>('http://localhost:3000/contacts')
+      .subscribe((data) => (this.contacts = data));
+  }
 }
