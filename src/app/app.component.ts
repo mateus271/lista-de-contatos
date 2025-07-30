ca8226df9647528d1b502e0a5cfeba20b5cd652e
Mateus Bullerjahn <mateusfb271@gmail.com>
2025-07-30 19:09:59 -0300

    Ajustes de estilo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,29 +12,35 @@ import { filter } from 'rxjs';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = "Agenda - Grupo II";
-  icon = "chevron_forward";
-  displayToolsMenu:boolean=true;
-  searchValue:string='';
-  
+  public title: string = "Agenda - Grupo II";
+  public icon: string = "chevron_forward";
+  public displayToolsMenu: boolean = true;
+  public searchValue: string = '';
+  public routeTitle: string = '';
+
  constructor(private matDialog: MatDialog,
-     private contactService: ContactService,
-    private router:Router,
+    private contactService: ContactService,
+    private router: Router,
   ) {
-      
-      this.router.events.pipe(
+    this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe((event: NavigationEnd) => {
-     
-        if(event.url==="/list"){
-          this.displayToolsMenu=true;
-        }else{
-          this.displayToolsMenu=false;
-        }
-    });
-    }
+      const currentUrl = this.router.url;
 
-   
+      if (currentUrl.includes('/list')) {
+        this.routeTitle = 'Lista de contatos';
+      } else if (currentUrl.includes('/about')) {
+        this.routeTitle = 'Sobre o aplicativo';
+      }
+
+      if (event.url === "/list") {
+        this.displayToolsMenu = true;
+      } else {
+        this.displayToolsMenu = false;
+      }
+    });
+  }
+
   public addNewContact(): void {
     this.matDialog.open(ContactModalComponent, {
       data: {
@@ -43,7 +49,6 @@ export class AppComponent {
       width: "500px",
       height: "500px"
     });
-    
   }
 
   public handleKeydown($event: Event) {
